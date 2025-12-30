5e836750edd9b54937b986c92c27b7a5431d2043
bullets-jyr <jy.ryu.jiyoung@gmail.com>
2025-12-30 17:09:39 +0900

    feat: Company info JSON parsing

--- lib/data/mapper/company_mapper.dart
+++ lib/data/mapper/company_mapper.dart
@@ -1,22 +1,28 @@
 import 'package:stock_app/data/source/local/company_listing_entity.dart';
+import 'package:stock_app/data/source/remote/dto/company_info_dto.dart';
+import 'package:stock_app/domain/model/company_info.dart';
 import 'package:stock_app/domain/model/company_listing.dart';
 
 extension ToCompanyListing on CompanyListingEntity {
   CompanyListing toCompanyListing() {
-    return CompanyListing(
-      symbol: symbol,
-      name: name,
-      exchange: exchange,
-    );
+    return CompanyListing(symbol: symbol, name: name, exchange: exchange);
   }
 }
 
 extension ToCompanyListingEntity on CompanyListing {
   CompanyListingEntity toCompanyListingEntity() {
-    return CompanyListingEntity(
-      symbol: symbol,
-      name: name,
-      exchange: exchange,
+    return CompanyListingEntity(symbol: symbol, name: name, exchange: exchange);
+  }
+}
+
+extension ToCompanyInfo on CompanyInfoDto {
+  CompanyInfo toCompanyInfo() {
+    return CompanyInfo(
+      symbol: symbol ?? '',
+      description: description ?? '',
+      name: name ?? '',
+      country: country ?? '',
+      industry: industry ?? '',
     );
   }
 }
