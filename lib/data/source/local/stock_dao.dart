3013a795b688c6416d0efbe7539cf6bb49849a1e
bullets-jyr <jy.ryu.jiyoung@gmail.com>
2025-12-30 13:36:28 +0900

    feat: Implement CompanyListings ViewModel

--- lib/data/source/local/stock_dao.dart
+++ lib/data/source/local/stock_dao.dart
@@ -4,26 +4,24 @@ import 'package:stock_app/data/source/local/company_listing_entity.dart';
 class StockDao {
   static const companyListing = 'companyListing';
 
-  final box = Hive.box('stock.db');
-
   // 추가
   Future<void> insertCompanyListings(
-    List<CompanyListingEntity> companyListingEntity,
+    List<CompanyListingEntity> companyListingEntities,
   ) async {
-    await box.put(StockDao.companyListing, companyListingEntity);
+    final box = await Hive.openBox<CompanyListingEntity>('stock.db');
+    await box.addAll(companyListingEntities);
   }
 
   // 클리어
   Future clearCompanyListings() async {
+    final box = await Hive.openBox<CompanyListingEntity>('stock.db');
     await box.clear();
   }
 
   // 검색
   Future<List<CompanyListingEntity>> searchCompanyListing(String query) async {
-    final List<CompanyListingEntity> companyListing = box.get(
-      StockDao.companyListing,
-      defaultValue: [],
-    );
+    final box = await Hive.openBox<CompanyListingEntity>('stock.db');
+    final List<CompanyListingEntity> companyListing = box.values.toList();
 
     return companyListing
         .where(
