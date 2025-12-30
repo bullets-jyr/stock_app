a31f0c8e84bc8ce82f13a01b752c05ce46143c71
bullets-jyr <jy.ryu.jiyoung@gmail.com>
2025-12-30 23:30:35 +0900

    fix: Fix graph bug

--- lib/presentation/company_info/components/stock_chart.dart
+++ lib/presentation/company_info/components/stock_chart.dart
@@ -6,17 +6,22 @@ import 'package:stock_app/domain/model/intraday_info.dart';
 
 class StockChart extends StatelessWidget {
   final List<IntradayInfo> infos;
-  final Color color;
+  final Color graphColor;
+  final Color textColor;
 
-  const StockChart({Key? key, this.infos = const [], required this.color})
-    : super(key: key);
+  const StockChart({
+    Key? key,
+    this.infos = const [],
+    required this.graphColor,
+    required this.textColor,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
       height: 250,
-      child: CustomPaint(painter: ChartPainter(infos, color)),
+      child: CustomPaint(painter: ChartPainter(infos, graphColor, textColor)),
     );
   }
 }
@@ -126,7 +131,9 @@ class ChartPainter extends CustomPainter {
   final List<IntradayInfo> infos;
 
   // 차트 선과 채우기 색상
-  final Color color;
+  final Color graphColor;
+
+  final Color textColor;
 
   // 차트에서 가장 높은 가격 값
   // map으로 close 값만 뽑고, fold로 최대값 계산 → 소수점 올림
@@ -143,11 +150,11 @@ class ChartPainter extends CustomPainter {
   late Paint strokePaint;
 
   // 생성자
-  ChartPainter(this.infos, this.color) {
+  ChartPainter(this.infos, this.graphColor, this.textColor) {
     // 선 스타일 정의
     strokePaint = Paint()
       ..color =
-          color // 선 색상
+          graphColor // 선 색상
       ..style = PaintingStyle
           .stroke // 채우지 않고 선만 그림
       ..strokeWidth =
@@ -168,7 +175,7 @@ class ChartPainter extends CustomPainter {
       final tp = TextPainter(
         text: TextSpan(
           text: '${(lowerValue + priceStep * i).round()}',
-          style: const TextStyle(fontSize: 12),
+          style: TextStyle(fontSize: 12, color: textColor),
         ),
         textAlign: TextAlign.start,
         textDirection: TextDirection.ltr,
@@ -189,7 +196,8 @@ class ChartPainter extends CustomPainter {
 
     // X축 시간 라벨을 위한 간격
     // 전체 너비를 데이터 개수로 나눔
-    final spacePerHour = size.width / infos.length;
+    // final spacePerHour = size.width / infos.length;
+    final spacePerHour = (size.width - spacing) / infos.length;
 
     // 12개 데이터마다 한 번씩 시간 표시 (예: 5분봉이면 1시간 간격)
     for (var i = 0; i < infos.length; i += 12) {
@@ -197,7 +205,10 @@ class ChartPainter extends CustomPainter {
       final hour = infos[i].date.hour;
 
       final tp = TextPainter(
-        text: TextSpan(text: '$hour', style: const TextStyle(fontSize: 12)),
+        text: TextSpan(
+          text: '$hour',
+          style: TextStyle(fontSize: 12, color: textColor),
+        ),
         textAlign: TextAlign.start,
         textDirection: TextDirection.ltr,
       );
@@ -205,13 +216,14 @@ class ChartPainter extends CustomPainter {
       tp.layout();
 
       // 시간 텍스트 하단에 그리기
-      tp.paint(
-        canvas,
-        Offset(
-          i * spacePerHour + spacing, // X 위치
-          size.height - 5, // 하단 근처
-        ),
-      );
+      // tp.paint(
+      //   canvas,
+      //   Offset(
+      //     i * spacePerHour + spacing, // X 위치
+      //     size.height - 5, // 하단 근처
+      //   ),
+      // );
+      tp.paint(canvas, Offset(i * spacePerHour + spacing, size.height + 20));
     }
 
     // 마지막 X 좌표 저장용 (채우기 영역 닫을 때 사용)
@@ -241,11 +253,13 @@ class ChartPainter extends CustomPainter {
 
       // 현재 포인트 좌표
       final x1 = spacing + i * spacePerHour;
-      final y1 = size.height - spacing - (leftRatio * size.height);
+      // final y1 = size.height - spacing - (leftRatio * size.height);
+      final y1 = size.height - (leftRatio * size.height).toDouble();
 
       // 다음 포인트 좌표
       final x2 = spacing + (i + 1) * spacePerHour;
-      final y2 = size.height - spacing - (rightRatio * size.height);
+      // final y2 = size.height - spacing - (rightRatio * size.height);
+      final y2 = size.height - (rightRatio * size.height).toDouble();
 
       // 첫 점이면 이동만
       if (i == 0) {
@@ -272,7 +286,7 @@ class ChartPainter extends CustomPainter {
         Offset.zero,
         Offset(0, size.height - spacing),
         [
-          color.withOpacity(0.5), // 위쪽 진한 색
+          graphColor.withOpacity(0.5), // 위쪽 진한 색
           Colors.transparent, // 아래쪽 투명
         ],
       );
